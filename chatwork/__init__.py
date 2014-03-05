366c7fe6931e94bdbdbd838f2f1c955ce6d0e28a
Shintaro Maeda <maesin@renjaku.jp>
2014-03-05 09:29:49 +0900

    Fix Python 3 support

--- chatwork/__init__.py
+++ chatwork/__init__.py
@@ -20,44 +20,50 @@ class Client:
     _room_members = None
 
     def me(self):
-        return self.__get(_url_me) 
+        return self._get(_url_me) 
 
     def my_status(self):
-        return self.__get(_url_my_status)
+        return self._get(_url_my_status)
 
     def my_tasks(self, **payload):
-        return self.__get(_url_my_tasks, payload=payload)
+        return self._get(_url_my_tasks, payload=payload)
 
     def visit(self, room_id):
         self._room_id = room_id
         return self
 
     def members(self):
-        self._room_members = {self._room_id: self.__get(_url_rooms_members.format(room_id=self._room_id))}
+        self._room_members = {self._room_id: self._get(_url_rooms_members.format(room_id=self._room_id))}
         return self._room_members[self._room_id]
 
     def post(self, body):
-        return self.__post(_url_rooms_messages.format(room_id=self._room_id), payload={"body": body})
+        """
+        Post a message in this room.
+
+        >>> auth('972608a5c871a0f76fac0e58f5c2871e').visit(17402654).post('test') is not None
+        True
+        """
+        return self._post(_url_rooms_messages.format(room_id=self._room_id), payload={"body": body})
 
     def post_to_all(self, body):
-        if self._room_members is None or not self._room_members.has_key(self._room_id):
+        if self._room_members is None or "self._room_id" not in self._room_members:
             self.members()
-        body = "\n".join([u"[To:{}] {}さん".format(m["account_id"], m["name"]) for m in self._room_members[self._room_id]]) + "\n" + body.decode("utf-8")
+        body = "\n".join([u"[To:{0}] {1}さん".format(m["account_id"], m["name"]) for m in self._room_members[self._room_id]]) + "\n" + body.decode("utf-8")
         return self.post(body)
 
-    def __get(self, url, **kwargs):
-        response = requests.get(url, params=kwargs["payload"] if kwargs.has_key("payload") else None, headers={"X-ChatWorkToken": self._token})
-        return self.__process_response(response)
+    def _get(self, url, **kwargs):
+        response = requests.get(url, params=kwargs["payload"] if "payload" in kwargs else None, headers={"X-ChatWorkToken": self._token})
+        return self._process_response(response)
 
-    def __post(self, url, **kwargs):
-        response = requests.post(url, data=kwargs["payload"] if kwargs.has_key("payload") else None, headers={"X-ChatWorkToken": self._token})
-        return self.__process_response(response)
+    def _post(self, url, **kwargs):
+        response = requests.post(url, data=kwargs["payload"] if "payload" in kwargs else None, headers={"X-ChatWorkToken": self._token})
+        return self._process_response(response)
 
-    def __process_response(self, response):
-        logging.debug("url: {}".format(response.url))
-        logging.debug("status_code: {}".format(response.status_code))
-        logging.debug("encoding: {}".format(response.encoding))
-        logging.debug("header: {}".format(response.headers))
-        logging.debug("text: {}".format(response.text))
+    def _process_response(self, response):
+        logging.debug("url: {0}".format(response.url))
+        logging.debug("status_code: {0}".format(response.status_code))
+        logging.debug("encoding: {0}".format(response.encoding))
+        logging.debug("header: {0}".format(response.headers))
+        logging.debug("text: {0}".format(response.text))
         return response.json() if response.status_code == 200 else None
 
