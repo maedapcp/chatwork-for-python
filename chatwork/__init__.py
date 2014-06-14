66a61d37a61a8d1d43e757f169f1bbeeea13f097
Shintaro Maeda <maesin@renjaku.jp>
2014-06-15 01:06:24 +0900

    Formalize all code in accordance with PEP8

--- chatwork/__init__.py
+++ chatwork/__init__.py
@@ -1,69 +1,164 @@
 # -*- coding: utf-8 -*-
 
+from easydict import EasyDict
+
 import logging
 import requests
 
-_url_me             = "https://api.chatwork.com/v1/me"
-_url_my_status      = "https://api.chatwork.com/v1/my/status"
-_url_my_tasks       = "https://api.chatwork.com/v1/my/tasks"
-_url_rooms_members  = "https://api.chatwork.com/v1/rooms/{room_id:d}/members"
-_url_rooms_messages = "https://api.chatwork.com/v1/rooms/{room_id:d}/messages" 
+_url_me = 'https://api.chatwork.com/v1/me'
+_url_my_status = 'https://api.chatwork.com/v1/my/status'
+_url_my_tasks = 'https://api.chatwork.com/v1/my/tasks'
+_url_rooms_members = 'https://api.chatwork.com/v1/rooms/{room_id:d}/members'
+_url_rooms_messages = 'https://api.chatwork.com/v1/rooms/{room_id:d}/messages'
 
-def auth(token):
-    c = Client()
-    c._token = token
-    return c
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class VagabondError(Error):
+    def __str__(self):
+        return 'Not visit the room yet.'
+
+
+class VeryHardError(Error):
+    def __str__(self):
+        return 'That obj is very hard not easy.'
+
+
+def _to_easy(obj):
+    if isinstance(obj, (list, tuple)):
+        return [EasyDict(x) for x in obj]
+    elif isinstance(obj, dict):
+        return EasyDict(obj)
+    raise VeryHardError()
+
 
 class Client:
-    _token = None
-    _room_id = None
-    _room_members = None
+    token = None
+    room_id = None
+
+    def __init__(self, token):
+        self.token = token
+
+    def __headers(self):
+        return {'X-ChatWorkToken': self.token}
 
     def me(self):
-        return self._get(_url_me) 
+        """
+        Get a self introduction.
+
+        >>> cli = Client('972608a5c871a0f76fac0e58f5c2871e')
+        >>> res = cli.me()
+        >>> res is not None
+        True
+        """
+        return self.__get(_url_me)
 
     def my_status(self):
-        return self._get(_url_my_status)
+        """
+        Get a my status.
+
+        >>> cli = Client('972608a5c871a0f76fac0e58f5c2871e')
+        >>> res = cli.my_status()
+        >>> res is not None
+        True
+        """
+        return self.__get(_url_my_status)
 
     def my_tasks(self, **payload):
-        return self._get(_url_my_tasks, payload=payload)
+        """
+        Get a my tasks.
+
+        >>> cli = Client('972608a5c871a0f76fac0e58f5c2871e')
+        >>> res = cli.my_tasks()
+        >>> res is not None
+        True
+        """
+        return self.__get(_url_my_tasks, payload=payload)
 
     def visit(self, room_id):
-        self._room_id = room_id
+        """
+        Visit the room.
+
+        >>> cli = Client('972608a5c871a0f76fac0e58f5c2871e')
+        >>> res = cli.visit(17402654)
+        >>> res is not None
+        True
+        """
+        self.room_id = room_id
         return self
 
     def members(self):
-        self._room_members = {self._room_id: self._get(_url_rooms_members.format(room_id=self._room_id))}
-        return self._room_members[self._room_id]
+        """
+        Get members.
+
+        >>> cli = Client('972608a5c871a0f76fac0e58f5c2871e')
+        >>> res = cli.visit(17402654).members()
+        >>> res is not None
+        True
+        """
+        if self.room_id is None:
+            raise VagabondError()
+        url = _url_rooms_members.format(room_id=self.room_id)
+        return self.__get(url)
 
     def post(self, body):
         """
         Post a message in this room.
 
-        >>> auth('972608a5c871a0f76fac0e58f5c2871e').visit(17402654).post('test') is not None
+        >>> cli = Client('972608a5c871a0f76fac0e58f5c2871e')
+        >>> res = cli.visit(17402654).post('test')
+        >>> res is not None
         True
         """
-        return self._post(_url_rooms_messages.format(room_id=self._room_id), payload={"body": body})
+        url = _url_rooms_messages.format(room_id=self.room_id)
+        return self.__post(url, payload={'body': body})
 
     def post_to_all(self, body):
-        if self._room_members is None or "self._room_id" not in self._room_members:
-            self.members()
-        body = "\n".join([u"[To:{0}] {1}さん".format(m["account_id"], m["name"]) for m in self._room_members[self._room_id]]) + "\n" + body.decode("utf-8")
+        """
+        Post a message to all members in this room.
+
+        >>> cli = Client('972608a5c871a0f76fac0e58f5c2871e')
+        >>> res = cli.visit(17402654).post_to_all('test to all')
+        >>> res is not None
+        True
+        """
+        if self.room_id is None:
+            raise VagabondError()
+        room_members = self.members()
+        to = u'[To:{0}] {1}さん'
+        to_all = [to.format(m['account_id'], m['name']) for m in room_members]
+        body = "\n".join(to_all) + "\n" + body.decode('utf-8')
         return self.post(body)
 
-    def _get(self, url, **kwargs):
-        response = requests.get(url, params=kwargs["payload"] if "payload" in kwargs else None, headers={"X-ChatWorkToken": self._token})
-        return self._process_response(response)
+    def __get(self, url, **kwargs):
+        params = kwargs['payload'] if 'payload' in kwargs else None
+        response = requests.get(url, params=params, headers=self.__headers())
+        return self.__process_response(response)
 
-    def _post(self, url, **kwargs):
-        response = requests.post(url, data=kwargs["payload"] if "payload" in kwargs else None, headers={"X-ChatWorkToken": self._token})
-        return self._process_response(response)
+    def __post(self, url, **kwargs):
+        data = kwargs['payload'] if 'payload' in kwargs else None
+        response = requests.post(url, data=data, headers=self.__headers())
+        return self.__process_response(response)
 
-    def _process_response(self, response):
-        logging.debug("url: {0}".format(response.url))
-        logging.debug("status_code: {0}".format(response.status_code))
-        logging.debug("encoding: {0}".format(response.encoding))
-        logging.debug("header: {0}".format(response.headers))
-        logging.debug("text: {0}".format(response.text))
-        return response.json() if response.status_code == 200 else None
+    def __process_response(self, response):
+        logging.debug('url: {0}'.format(response.url))
+        logging.debug('status_code: {0}'.format(response.status_code))
+        logging.debug('encoding: {0}'.format(response.encoding))
+        logging.debug('header: {0}'.format(response.headers))
+        logging.debug('text: {0}'.format(response.text))
+        if response.status_code == 200:
+            json = response.json()
+            return _to_easy(json)
+        return None
 
+
+def auth(token):
+    return Client(token)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ try:
     import setuptools
     from setuptools import setup, find_packages
 except ImportError:
-    print("Please install setuptools.")
+    print('Please install setuptools.')
 
     def find_scripts(scripts_path):
         base_path = os.path.abspath(scripts_path)
-        return list(map(lambda path: os.path.join(scripts_path, path),
-                filter(lambda file_name: os.path.isfile(
-                    os.path.join(base_path, file_name)), os.listdir(base_path)
-               )))
+        make_path = lambda path: os.path.join(scripts_path, path)
+        is_file = lambda file_name: os.path.isfile(
+            os.path.join(base_path, file_name))
+        return list(map(make_path, filter(is_file, os.listdir(base_path))))
 
 import os
 import sys
 
-libdir = "."
+libdir = '.'
 
 sys.path.insert(0, libdir)
 
@@ -25,11 +25,13 @@ import info
 import version
 
 setup_options = info.INFO
-setup_options["version"] = version.VERSION
-setup_options.update(dict(
-    install_requires = open("requirements.txt").read().splitlines(),
-    packages         = find_packages(libdir),
-    package_dir      = {"": libdir},
-))
+setup_options['version'] = version.VERSION
+setup_options.update(
+    {
+        'install_requires': open('requirements.txt').read().splitlines(),
+        'packages': find_packages(libdir),
+        'package_dir': {'': libdir},
+    }
+)
 
 setup(**setup_options)
